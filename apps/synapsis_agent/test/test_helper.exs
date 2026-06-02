92348580ac63a04660b78b114ac0cec4daada363
Jonathan Gao <gsmlg.com@gmail.com>
2026-06-03 05:50:23 +0800

    style(c4): mix format test_helper

--- apps/synapsis_agent/test/test_helper.exs
+++ apps/synapsis_agent/test/test_helper.exs
@@ -1,4 +1,3 @@
-
 # Start RunRegistry for tests if not already started (may be started by synapsis_core)
 case Registry.start_link(keys: :unique, name: Synapsis.Agent.Runtime.RunRegistry) do
   {:ok, _} -> :ok
