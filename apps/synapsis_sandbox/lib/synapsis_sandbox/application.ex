347de0b5273b7958cfaa86c0596b2ab631a549c2
Jonathan Gao <gsmlg.com@gmail.com>
2026-06-15 17:59:08 +0800

    chore(sandbox): scaffold synapsis_sandbox app

---
+++ apps/synapsis_sandbox/lib/synapsis_sandbox/application.ex
@@ -0,0 +1,11 @@
+defmodule SynapsisSandbox.Application do
+  @moduledoc false
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = []
+    opts = [strategy: :one_for_one, name: SynapsisSandbox.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ apps/synapsis_sandbox/mix.exs
@@ -0,0 +1,32 @@
+defmodule SynapsisSandbox.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :synapsis_sandbox,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.18",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger], mod: {SynapsisSandbox.Application, []}]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:synapsis_core, in_umbrella: true},
+      {:jason, "~> 1.4"}
+    ]
+  end
+end
