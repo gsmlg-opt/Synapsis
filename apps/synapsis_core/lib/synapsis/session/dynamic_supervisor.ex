4399b1dc887c8c987ab92aca8cc8c7b964633d8b
Jonathan Gao <gsmlg.com@gmail.com>
2026-09-05 01:45:00 +0800

    fix(sessions): await registry teardown on stop

--- apps/synapsis_core/lib/synapsis/session/dynamic_supervisor.ex
+++ apps/synapsis_core/lib/synapsis/session/dynamic_supervisor.ex
@@ -2,6 +2,8 @@ defmodule Synapsis.Session.DynamicSupervisor do
   @moduledoc "DynamicSupervisor for starting/stopping session process trees."
   use DynamicSupervisor
 
+  @registry_release_timeout 5_000
+
   def start_link(_opts) do
     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
@@ -30,10 +32,38 @@ defmodule Synapsis.Session.DynamicSupervisor do
   def stop_session(session_id) do
     case Registry.lookup(Synapsis.Session.SupervisorRegistry, session_id) do
       [{pid, _}] ->
-        DynamicSupervisor.terminate_child(__MODULE__, pid)
+        case DynamicSupervisor.terminate_child(__MODULE__, pid) do
+          :ok -> await_registry_release(session_id)
+          error -> error
+        end
 
       [] ->
         {:error, :not_found}
     end
+  end
+
+  defp await_registry_release(session_id) do
+    deadline = System.monotonic_time(:millisecond) + @registry_release_timeout
+    do_await_registry_release(session_id, deadline)
+  end
+
+  defp do_await_registry_release(session_id, deadline) do
+    if registered?(session_id) do
+      if System.monotonic_time(:millisecond) < deadline do
+        receive do
+        after
+          1 -> do_await_registry_release(session_id, deadline)
+        end
+      else
+        {:error, :registry_release_timeout}
+      end
+    else
+      :ok
+    end
+  end
+
+  defp registered?(session_id) do
+    Registry.lookup(Synapsis.Session.SupervisorRegistry, session_id) != [] or
+      Registry.lookup(Synapsis.Session.Registry, session_id) != []
   end
 end
